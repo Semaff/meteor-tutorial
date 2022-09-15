229e530e846fcc30dfdbd0e3753ac133ad7a6ff6
Semaff <semarc-zhu@yandex.ru>
2022-09-15 21:34:32 +0300

    Created pendingTasksCount and filter button

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Task from './Task';
 import { useTracker } from "meteor/react-meteor-data";
 import { TasksCollection } from '../api/TasksCollection';
@@ -17,21 +17,41 @@ const deleteTask = ({ _id }) => {
 }
 
 export const App = () => {
+    const [hideCompleted, setHideCompleted] = useState(false);
+    const hideCompletedFilter = { isChecked: { $ne: true } };
+
     /* "useTracker" is something like "subscribe" in Redux */
-    const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+    const tasks = useTracker(() => {
+        return TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, { sort: { createdAt: -1 } }).fetch()
+    });
+
+    const pendingTasksCount = useTracker(() => {
+        return TasksCollection.find(hideCompletedFilter).count();
+    });
+
+    const pendingTasksTitle = `${pendingTasksCount ? ` (${pendingTasksCount})` : ''}`;
 
     return (
         <div className="app">
             <header>
                 <div className="app-bar">
                     <div className="app-header">
-                        <h1>📝️ ToDo List</h1>
+                        <h1>
+                            📝️ ToDo List
+                            {pendingTasksTitle}
+                        </h1>
                     </div>
                 </div>
             </header>
 
             <div className="main">
                 <TaskForm />
+
+                <div className="filter">
+                    <button onClick={() => setHideCompleted(prev => !prev)}>
+                        {hideCompleted ? "Show All" : "Hide Completed"}
+                    </button>
+                </div>
 
                 <ul className="tasks">
                     {tasks.map(task => (
