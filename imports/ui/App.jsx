3b89435fc4d866809dc865c198e897c2da0f7123
Semaff <semarc-zhu@yandex.ru>
2022-09-15 21:24:54 +0300

    Created functions to update and delete task

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,6 +4,18 @@ import { useTracker } from "meteor/react-meteor-data";
 import { TasksCollection } from '../api/TasksCollection';
 import TaskForm from './TaskForm';
 
+const toggleChecked = ({ _id, isChecked }) => {
+    TasksCollection.update(_id, {
+        $set: {
+            isChecked: !isChecked
+        }
+    })
+};
+
+const deleteTask = ({ _id }) => {
+    TasksCollection.remove(_id);
+}
+
 export const App = () => {
     /* "useTracker" is something like "subscribe" in Redux */
     const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
@@ -16,7 +28,12 @@ export const App = () => {
 
             <ul>
                 {tasks.map(task => (
-                    <Task key={task._id} task={task} />
+                    <Task
+                        key={task._id}
+                        task={task}
+                        onCheckBoxClick={toggleChecked}
+                        onDeleteClick={deleteTask}
+                    />
                 ))}
             </ul>
         </div>
