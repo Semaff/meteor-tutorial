1f98847cc9be095de09b6ba8b241a50ea580c818
Semaff <semarc-zhu@yandex.ru>
2022-09-15 21:19:07 +0300

    Made App to use DB tasks and added TaskForm

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import Task from './Task';
+import { useTracker } from "meteor/react-meteor-data";
+import { TasksCollection } from '../api/TasksCollection';
+import TaskForm from './TaskForm';
 
-const tasks = [
-    { _id: 1, text: 'First Task' },
-    { _id: 2, text: 'Second Task' },
-    { _id: 3, text: 'Third Task' },
-]
+export const App = () => {
+    /* "useTracker" is something like "subscribe" in Redux */
+    const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
 
-export const App = () => (
-    <div>
-        <h1>Welcome to Meteor!</h1>
+    return (
+        <div>
+            <h1>Welcome to Meteor!</h1>
 
-        <ul>
-            {tasks.map(task => (
-                <Task key={task._id} task={task} />
-            ))}
-        </ul>
-    </div>
-);
+            <TaskForm />
+
+            <ul>
+                {tasks.map(task => (
+                    <Task key={task._id} task={task} />
+                ))}
+            </ul>
+        </div>
+    )
+};
