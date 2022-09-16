f076ce7e5b0e81d553485f00442f0a52c6de01a5
Semaff <semarc-zhu@yandex.ru>
2022-09-16 10:34:34 +0300

    Updated to use MUI

---
+++ imports/ui/Auth/LoginForm.jsx
@@ -0,0 +1,56 @@
+import { Meteor } from 'meteor/meteor';
+import React, { useState } from 'react';
+import LoginWithGitHub from './LoginWithGitHub';
+import { Box, Button, Input, InputLabel } from "@mui/material";
+
+const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submit = e => {
+        e.preventDefault();
+        Meteor.loginWithPassword(username, password);
+    };
+
+    return (
+        <Box className="display-center">
+            <Box className="box">
+                <form onSubmit={submit} className="form">
+                    <LoginWithGitHub />
+
+                    <InputLabel sx={{ color: "black" }} htmlFor='username'>
+                        Username
+                    </InputLabel>
+
+                    <Input
+                        type="text"
+                        placeholder="Username"
+                        id="username"
+                        required
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+
+                    <InputLabel sx={{ color: "black" }} htmlFor='password'>
+                        Password
+                    </InputLabel>
+
+                    <Input
+                        type="password"
+                        placeholder="******"
+                        id="password"
+                        required
+                        value={password}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+
+                    <Button type='submit' variant='contained'>
+                        Log In
+                    </Button>
+                </form>
+            </Box>
+        </Box>
+    )
+}
+
+export default LoginForm;
