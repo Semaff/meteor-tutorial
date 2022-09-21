07f680ae6d97af64d920d039ccf16138060b8e69
Semaff <semarc-zhu@yandex.ru>
2022-09-21 19:47:08 +0300

    Removed unused imports

--- imports/ui/Task/TaskList.jsx
+++ imports/ui/Task/TaskList.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import List from '@mui/material/List';
 import Task from './Task';
 import { useTracker } from "meteor/react-meteor-data";
