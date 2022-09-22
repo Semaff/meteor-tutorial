1397e2299cc117ecb8fc659623555a0584714066
Semaff <semarc-zhu@yandex.ru>
2022-09-22 19:08:21 +0300

    Removed commented code

--- imports/ui/Task/TaskList.jsx
+++ imports/ui/Task/TaskList.jsx
@@ -1,10 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import List from '@mui/material/List';
 import Task from './Task';
 import { Box, Typography } from '@mui/material';
 import { Meteor } from "meteor/meteor";
-// import { useTracker } from "meteor/react-meteor-data";
-// import { TagsCollection } from '../../api/tags/TagsCollection';
 
 const toggleChecked = ({ _id, isChecked }) => {
     Meteor.call('tasks.setIsChecked', _id, !isChecked);
@@ -19,36 +17,6 @@ const changeTaskTags = (taskId, tags) => {
 }
 
 const TaskList = ({ tasks }) => {
-    // const [tags, setTags] = useState([]);
-    // const [input, setInput] = useState("");
-    // const [timer, setTimer] = useState(null);
-
-    // useEffect(() => {
-    //     const handler = Meteor.subscribe("tags");
-
-    //     if (timer) {
-    //         clearTimeout(timer);
-    //         setTimer(null);
-    //     }
-
-    //     if (input.length <= 1) {
-    //         setTags(TagsCollection.find().fetch().slice(0, 10));
-    //     } else {
-    //         const timerEl = setTimeout(() => {
-    //             setTags(TagsCollection.find({
-    //                 text: {
-    //                     $regex: input,
-    //                     $options: "gi"
-    //                 }
-    //             }).fetch());
-    //         }, 300);
-
-    //         setTimer(timerEl);
-    //     }
-
-    //     return () => handler.stop();
-    // }, [input]);
-
     return (
         <Box className='box'>
             {tasks.length > 0
@@ -58,11 +26,9 @@ const TaskList = ({ tasks }) => {
                             <Task
                                 key={task._id}
                                 task={task}
-                                // tags={tags}
                                 onCheckBoxClick={toggleChecked}
                                 onDeleteClick={deleteTask}
                                 onTagsChange={changeTaskTags}
-                                // onInputChange={setInput}
                             />
                         ))}
                     </List>
