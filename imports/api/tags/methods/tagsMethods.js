edd6fe824c8567c949b6ee0ba89d7b92b6b9f256
Semaff <semarc-zhu@yandex.ru>
2022-09-22 18:37:15 +0300

    Created getAll tags method

--- imports/api/tags/methods/tagsMethods.js
+++ imports/api/tags/methods/tagsMethods.js
@@ -3,6 +3,20 @@ import { check } from "meteor/check";
 import { TagsCollection } from "../TagsCollection";
 
 Meteor.methods({
+    'tags.getAll'({ query, limit } = {}) {
+        const fields = {};
+
+        if (query) {
+            fields.text = {
+                $regex: query,
+                $options: "gi"
+            }
+        }
+
+        const tags = TagsCollection.find(fields, { limit }).fetch();
+        return tags;
+    },
+
     'tags.insert'(tag) {
         check(tag, String);
 
