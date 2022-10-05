959a8498cee2bc12728e492a01597fbd9ca4c872
Semaff <semarc-zhu@yandex.ru>
2022-10-05 22:43:28 +0300

    Created SIgnIn page

---
+++ imports/ui/pages/SignIn.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import LoginForm from '../components/Auth/LoginForm';
+
+const SignInPage = () => {
+  return (
+    <LoginForm />
+  )
+}
+
+export default SignInPage;
