3174d418c7dcf5a9b8663d50ae1031a96f4c339e
Semaff <semarc-zhu@yandex.ru>
2022-10-06 20:54:35 +0300

    Structured imports

--- imports/ui/components/Navbar/Navbar.tsx
+++ imports/ui/components/Navbar/Navbar.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import { Button, Typography, Toolbar, Box, AppBar } from "@mui/material";
 import { Meteor } from "meteor/meteor";
 
 interface NavbarProps {
