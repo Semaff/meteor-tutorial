6a45387bce8bf61be7d9bec6204a3a451ff1e6b1
Semaff <semarc-zhu@yandex.ru>
2022-10-11 16:08:06 +0300

    Updated SignUp page to be able to navigate to SIgnIn

--- imports/ui/pages/SignUp.tsx
+++ imports/ui/pages/SignUp.tsx
@@ -1,10 +1,18 @@
-import * as React from 'react';
-import LoginForm from '../components/Auth/LoginForm';
+import * as React from "react";
+import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
+import RegistrationForm from "../components/Auth/RegistrationForm";
+import { RouteNames } from "../router/routes";
 
 const SignUpPage = () => {
   return (
-    <LoginForm />
-  )
-}
+    <Box className="display-center">
+      <RegistrationForm />
+      <Link to={RouteNames.SIGNIN} className="auth__link">
+        Already have an account? Sign In.
+      </Link>
+    </Box>
+  );
+};
 
 export default SignUpPage;
